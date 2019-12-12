fec564cbd2da2545ffadd7be7315b1b61a63228e
Xris4o0 <xris4o0@gmail.com>
2019-12-11 21:08:15 -0500

    Added Lab 6

---
+++ Labs/2018/Lab6/exercise-2/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.listen(3000);
+
+app.get('/greet', function(req, res){
+    console.log('GET recieved: ' + req.requestTime);
+    res.send('hello world!');
+});
+
+var requestTime = function(req, res, next){
+    req.requestTime = Date.now()
+    next()
+}
+
+app.use(requestTime)
---
+++ Labs/2018/Lab6/exercise-3/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('');
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+
+mongoose.connect('mongodb://,dbuser>:<dbpassword>@ds125932.mlab.com:25932/testdb');
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({ name: String, status: String });
+
+var User = mongoose.model('User', userSchema);
+
+var john = User({ name: 'John', status: 'active' });
+
+john.save(function(err){
+    if(err) throw err;
+
+    console.log('**** User saved! ****');
+});
